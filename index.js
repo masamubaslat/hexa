da6404996cb0e4874e68b4e330f0ab8925cf87a7
masamubaslat <masa.mubaslat111#gmail.com>
2023-01-13 23:55:49 +0200

    initila version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const MailMessage = require('nodemailer/lib/mailer/mail-message');
 const app = express();
-const port=5000;
+const port=5000||process.env.PORT;
 app.use(cors());
 app.use(express.json({limit:"25mb"}));
 app.use(express.urlencoded({limit:"25mb"}));
